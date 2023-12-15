0c8b3ab6616afaec5a7d182918557aa6cf53b98b
AndreiDiaconuBitnovel <andrei.diaconu@bitnovel.com>
2023-12-15 19:06:59 +0200

    add jwtToken generation

---
+++ Utils/JwtToken.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApplication2.Utils
+{
+    public static class JwtToken
+    {
+        public static string CreateToken(string userName, IConfiguration config)
+        {
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim("userName", userName ),
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                config.GetSection("JwtConfig:Secret").Value!));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+    }
+}
